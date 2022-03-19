66ea2851c072563324b4bd689ac6d75fda272f41
manu-yaff <manuelyg2097@gmail.com>
2022-03-18 22:23:57 -0600

    add comments to code

--- lexer.py
+++ lexer.py
@@ -1,8 +1,33 @@
+"""
+Program that takes a grammar as input and produce the terminal and non terminal
+symbols
+
+Input example:
+8
+E -> T EPrime
+EPrime -> + T EPrime
+EPrime -> ' '
+T -> F TPrime
+TPrime -> * F TPrime
+TPrime -> ' '
+F -> ( E )
+F -> id
+
+Output example:
+Terminal: +, *, (, ), id
+Non terminal: E, EPrime T, TPrime, F
+
+
+Author: Manuel Yafté
+"""
+
+# read input filename
 print('Ingresa el nombre del archivo que contiene el input:')
 file = input()
 input_file = open('test_cases/' + file, 'r')
 lines = input_file.readlines()
 
+# list to store the initial terminals and non_terminals
 non_terminals = []
 temp_terminal = []
 
@@ -14,18 +39,18 @@ for index, line in enumerate(lines):
     non_terminals.append(splitted[0].strip())
     temp_terminal.append(splitted[1].strip())
 
-# split terminals
+# split terminals and added them to a list
 terminals = []
 for item in temp_terminal:
   if (item != "' '"):
     for element in item.split():
       terminals.append(element)
 
-# delete duplicates
+# remove duplicates from both lists
 non_terminals = list(dict.fromkeys(non_terminals))
 terminals = list(dict.fromkeys(terminals))
 
-# deleting false terminals
+# remove 'false' terminals
 items_to_delete = []
 for item in terminals:
   if(non_terminals.count(item)):
@@ -34,6 +59,7 @@ for item in terminals:
 for item in items_to_delete:
   terminals.remove(item)
 
+# format the output
 formatted_terminals = ", ".join(map(str, terminals))
 print('Terminal:', formatted_terminals)
 
