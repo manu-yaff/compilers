5a68129dece338cbbc57c7c49fdf960a8fafbca8
manu-yaff <manuelyg2097@gmail.com>
2022-04-23 19:49:28 -0500

    Add read terminals function and finish refactor

--- lexer.py
+++ lexer.py
@@ -41,47 +41,24 @@ def read_non_terminals(lines):
     if (index!= 0):
       terminal = line.split('->')[0].strip()
       non_terminals.append(terminal)
-
-
   return set(non_terminals)
+
+# read terminals
+def read_terminals(lines, non_terminals_set):
+  terminals = []
+  for index, line in enumerate(lines):
+    if (index != 0):
+      item = line.split('->')[1].strip()
+      if (item != "' '"):
+        for element in item.split():
+          if(element not in non_terminals_set):
+            terminals.append(element)
+  return set(terminals)
 
 
 print_menu()
 lines = read_file()
-print(read_non_terminals(lines))
+non_terminals = read_non_terminals(lines)
+print('Terminal: ', read_terminals(lines, non_terminals))
+print('Non terminal: ', non_terminals)
 
-
-# # read lines from file
-# for index, line in enumerate(lines):
-#   if (index != 0):
-#     line = line
-#     splitted = line.split('->')
-#     non_terminals.append(splitted[0].strip())
-#     temp_terminal.append(splitted[1].strip())
-
-# # split terminals and added them to a list
-# terminals = []
-# for item in temp_terminal:
-#   if (item != "' '"):
-#     for element in item.split():
-#       terminals.append(element)
-
-# # remove duplicates from both lists
-# non_terminals = list(dict.fromkeys(non_terminals))
-# terminals = list(dict.fromkeys(terminals))
-
-# # remove 'false' terminals
-# items_to_delete = []
-# for item in terminals:
-#   if(non_terminals.count(item)):
-#     items_to_delete.append(item)
-
-# for item in items_to_delete:
-#   terminals.remove(item)
-
-# # format the output
-# formatted_terminals = ", ".join(map(str, terminals))
-# print('Terminal:', formatted_terminals)
-
-# formatted_non_terminals = ", ".join(map(str, non_terminals))
-# print('Non terminal:', formatted_non_terminals)
