35a039f07cf90ad149cc6b04e2d8740164f9334f
manu-yaff <manuelyg2097@gmail.com>
2022-04-23 19:17:04 -0500

    Add function to print menu, function to read file and function to read non terminals

--- lexer.py
+++ lexer.py
@@ -21,48 +21,67 @@ Non terminal: E, EPrime T, TPrime, F
 Author: Manuel Yafté
 """
 
-# read input filename
-print('Ingresa el nombre del archivo que contiene el input:')
-print("['input1.txt', 'input2.txt', 'input3.txt', 'input4.txt', 'input5.txt']")
-file = input()
-input_file = open('test_cases/' + file, 'r')
-lines = input_file.readlines()
+# function to print menu
+def print_menu():
+  print('Ingresa el nombre del archivo que contiene el input:')
+  print("['input1.txt', 'input2.txt', 'input3.txt', 'input4.txt', 'input5.txt']")
 
-# list to store the initial terminals and non_terminals
-non_terminals = []
-temp_terminal = []
 
-# read lines from file
-for index, line in enumerate(lines):
-  if (index != 0):
-    line = line
-    splitted = line.split('->')
-    non_terminals.append(splitted[0].strip())
-    temp_terminal.append(splitted[1].strip())
+# function to read files
+def read_file():
+  file_name = input()
+  input_file = open('test_cases/' + file_name, 'r')
+  return input_file.readlines()
 
-# split terminals and added them to a list
-terminals = []
-for item in temp_terminal:
-  if (item != "' '"):
-    for element in item.split():
-      terminals.append(element)
+  
+# read non-terminals
+def read_non_terminals(lines):
+  non_terminals = []
+  for index, line in enumerate(lines):
+    if (index!= 0):
+      terminal = line.split('->')[0].strip()
+      non_terminals.append(terminal)
 
-# remove duplicates from both lists
-non_terminals = list(dict.fromkeys(non_terminals))
-terminals = list(dict.fromkeys(terminals))
 
-# remove 'false' terminals
-items_to_delete = []
-for item in terminals:
-  if(non_terminals.count(item)):
-    items_to_delete.append(item)
+  return set(non_terminals)
 
-for item in items_to_delete:
-  terminals.remove(item)
 
-# format the output
-formatted_terminals = ", ".join(map(str, terminals))
-print('Terminal:', formatted_terminals)
+print_menu()
+lines = read_file()
+print(read_non_terminals(lines))
 
-formatted_non_terminals = ", ".join(map(str, non_terminals))
-print('Non terminal:', formatted_non_terminals)
+
+# # read lines from file
+# for index, line in enumerate(lines):
+#   if (index != 0):
+#     line = line
+#     splitted = line.split('->')
+#     non_terminals.append(splitted[0].strip())
+#     temp_terminal.append(splitted[1].strip())
+
+# # split terminals and added them to a list
+# terminals = []
+# for item in temp_terminal:
+#   if (item != "' '"):
+#     for element in item.split():
+#       terminals.append(element)
+
+# # remove duplicates from both lists
+# non_terminals = list(dict.fromkeys(non_terminals))
+# terminals = list(dict.fromkeys(terminals))
+
+# # remove 'false' terminals
+# items_to_delete = []
+# for item in terminals:
+#   if(non_terminals.count(item)):
+#     items_to_delete.append(item)
+
+# for item in items_to_delete:
+#   terminals.remove(item)
+
+# # format the output
+# formatted_terminals = ", ".join(map(str, terminals))
+# print('Terminal:', formatted_terminals)
+
+# formatted_non_terminals = ", ".join(map(str, non_terminals))
+# print('Non terminal:', formatted_non_terminals)
