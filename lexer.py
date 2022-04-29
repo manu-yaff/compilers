a63f3c4c77e835f5771dbcfc46d019eedb6cfe15
manu-yaff <manuelyg2097@gmail.com>
2022-04-29 02:19:00 -0500

    Add first set hardcoded

--- lexer.py
+++ lexer.py
@@ -53,12 +53,79 @@ def read_terminals(lines, non_terminals_set):
         for element in item.split():
           if(element not in non_terminals_set):
             terminals.append(element)
+      else:
+        terminals.append("epsilon")
   return set(terminals)
+
+# read grammar rules and replace epsilon ocurrences
+def read_rules(lines):
+  lines_formatted = []
+  for index, line in enumerate(lines):
+    if (index != 0):
+      left_side = line.split('->')[0].strip()
+      right_side = line.split('->')[1].strip()
+      if (right_side == "' '"):
+        lines_formatted.append({ "left_side": left_side, "right_side": ["epsilon"] })
+      else:
+        lines_formatted.append({ "left_side": left_side,  "right_side": right_side.split() })
+  # need to format the symbols as arrays
+  return lines_formatted
+
+# generate FIRST Set
+def generate_first_set(non_terminal, productions, terminals, counter, first):
+  print(non_terminal, counter)
+  if (counter > 7):
+    print(first)
+    return first
+  
+  if (productions[counter]['left_side'] == non_terminal):
+    # aqui abajo esta el problema
+    if (productions[counter]['right_side'][0] in terminals):
+      print('es un terminal')
+      first.append(productions[counter]['right_side'][0])
+      generate_first_set(non_terminal, productions, terminals, counter + 1, first)
+    else:
+      generate_first_set(productions[counter]['right_side'][0], productions, terminals, counter + 1, first)
+  else:
+    generate_first_set(non_terminal, productions, terminals, counter + 1, first)
+  
+  # if (productions[counter]['left_side'] == symbol):
+  #   if (productions[counter]['left_side'] in terminals):
+  #     print(symbol)
+  #   else:
+  #     print('llamada con, ', productions[counter]['right_side'][0], counter + 1)
+  # else:
+  #   print('llamada con, ', symbol, productions, terminals, counter + 1)
+
+
+
+
+
+  # first = []
+  # symbol = rule["right_side"][0]
+  # if (symbol in terminals and ):
+  #   first.append(symbol)
+  # else:
+  #   generate_first_set(productions[i + 1], productions, terminals)
+
+  # print(first)
+  
+
 
 
 print_menu()
 lines = read_file()
+rules = read_rules(lines)
 non_terminals = read_non_terminals(lines)
-print('Terminal: ', read_terminals(lines, non_terminals))
-print('Non terminal: ', non_terminals)
+terminals = read_terminals(lines, non_terminals)
+print(terminals)
+print(rules)
 
+
+generate_first_set(rules[6]['left_side'], rules, terminals, 0, [])
+
+
+# print('Non terminals: ', non_terminals)
+# print('Terminals: ', terminals)
+
+# generate_first_set(grammar_rules, terminals, non_terminals)
